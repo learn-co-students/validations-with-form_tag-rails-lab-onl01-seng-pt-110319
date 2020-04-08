5837ee24bbd2233ecf1385333ee36283c00d9965
dannyflatiron <dannyflatiron@gmail.com>
2020-04-07 21:46:32 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,9 +8,35 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    # if @author = Author.create!(author_params)
+    #   redirect_to author_path(@author)
+    # else
+    #   render :new
+    # end
+    @author = Author.new(author_params)
 
-    redirect_to author_path(@author)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+    
+  end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else 
+      render :edit
+    end
+
+    
   end
 
   private
